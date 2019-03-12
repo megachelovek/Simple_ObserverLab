73a0922f777b184aa3897ff61da30aaafae30231
Danila Malchikov <2regvk@mail.ru>
2019-03-12 23:44:26 +0400

    fix

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -23,18 +24,21 @@ import java.awt.event.MouseListener;
 import static com.sun.java.accessibility.util.AWTEventMonitor.addMouseListener;
 
 
-public class Main extends Application implements MouseListener {
+public class Main extends Application  {
     private static final String standart = "file:src\\picture\\standart.png";
     private static final String leftEye = "file:src\\picture\\leftEye.png";
     private static final String rightEye = "file:src\\picture\\rightEye.png";
     private static final String mouth = "file:src\\picture\\mouth.png";
     private static final String nose = "file:src\\picture\\nose.png";
 
+    private static ImageView imageView;
+    private static Image image;
+    private static Scene scene;
+
     @Override
     public void start(Stage stage) throws Exception{
-        Image image = new Image(standart);
-
-        ImageView imageView = new ImageView(image);
+        image = new Image(standart);
+        imageView = new ImageView(image);
         imageView.setX(0);
         imageView.setY(0);
         imageView.setFitHeight(250);
@@ -42,8 +46,10 @@ public class Main extends Application implements MouseListener {
 
         imageView.setPreserveRatio(true);
         Group root = new Group(imageView);
-        imageView.addEventHandler(MouseListener);
-        Scene scene = new Scene(root, 250, 250);
+        imageView.setOnMouseClicked(click -> {
+            UpdateImage(click.getX(),click.getY());
+        } );
+        scene = new Scene(root, 250, 250);
         stage.setTitle("ObserverTest");
         stage.setScene(scene);
 
@@ -54,32 +60,18 @@ public class Main extends Application implements MouseListener {
 
     public static void main(String[] args) {
         launch(args);
-        blankArea.addMouseListener(this);
-        addMouseListener();
     }
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-
+    private static void UpdateImage(double x,double y){
+        if ( 10<x && x<90 && 10<y && y<90 ) {
+            String tt=image.toString();
+            if (image.equals(new Image(standart))  )  {
+                image = new Image(leftEye);
+            }else{
+                image = new Image(standart);
+            }
+        }
     }
 
-    @Override
-    public void mousePressed(MouseEvent e) {
 
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
-    }
 }
